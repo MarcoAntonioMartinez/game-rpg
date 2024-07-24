7d6dd2211895b25f55f57ec373b8b651e499cb64
Marco Martinez <marco.antoni.martinez@gmail.com>
2024-07-24 14:55:54 -0500

    Initialized project files

---
+++ placeholder.py
@@ -0,0 +1,4 @@
+# this will be the main game stuff where everything all comes together
+# rpg turn based text based
+# a bit of if statements
+#need to come up with a better name for the file
---
+++ weapons_class.py
@@ -0,0 +1,21 @@
+class Weapons: 
+
+    def _init_(weapon, weaponName, weaponDmg, weaponType, weaponRarity):
+        weapon.weaponName = weaponName
+        weapon.weaponDmg = weaponDmg
+        weapon.weaponType = weaponType
+        weapon.weaponName = weaponRarity
+
+# #name like Old Rusty Knife
+# weaponName
+
+# #how much damage does the weapon deal
+# weaponDmg
+
+# #is it ranged? is it close combat? is it a magical weapon?
+# weaponType
+
+# #how rare is it? can it be found in chests? enemy drops? given as reward?
+# weaponRarity
+
+
