7f62a3f52aa8d8b06c9c74f3abde6521d70181bf
Marco Martinez <marco.antoni.martinez@gmail.com>
2024-07-30 16:54:41 -0500

    Added some weapons that could be used and finalized the weapons class

---
+++ available_weapons.py
@@ -0,0 +1,7 @@
+from weapons_class import Weapons
+
+# weaponName, weaponDmg, weaponType, weaponRarity
+old_rusty_knife = Weapons('Old Rusty Knife', 10, 'Melee', 1)
+
+wooden_staff = Weapons('Wooden Staff', 5, 'Magic', 1)
+
--- weapons_class.py
+++ weapons_class.py
@@ -1,21 +1,30 @@
 class Weapons: 
 
-    def _init_(weapon, weaponName, weaponDmg, weaponType, weaponRarity):
-        weapon.weaponName = weaponName
-        weapon.weaponDmg = weaponDmg
-        weapon.weaponType = weaponType
-        weapon.weaponName = weaponRarity
+    def __init__(self, weapon_name, weapon_dmg, weapon_type, weapon_rarity):
+        
+        #name like Old Rusty Knife        
+        self.weapon_name = weapon_name
+        
+        #how much damage does the weapon deal      
+        self.weapon_dmg = weapon_dmg
+        
+        #is it ranged? is it close combat? is it a magical weapon?
+        #  two handed? should also have shield somewhere      
+        self.weapon_type = weapon_type
+        
+        
+        self.weapon_rarity = weapon_rarity
 
-# #name like Old Rusty Knife
-# weaponName
 
-# #how much damage does the weapon deal
+
+
+# 
 # weaponDmg
 
-# #is it ranged? is it close combat? is it a magical weapon?
+# 
 # weaponType
 
 # #how rare is it? can it be found in chests? enemy drops? given as reward?
 # weaponRarity
-
+  
 
