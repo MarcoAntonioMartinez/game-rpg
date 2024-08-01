cf979cc2ec5b4c0e808515a58c57211a111f500a
Marco Martinez <marco.antoni.martinez@gmail.com>
2024-08-01 16:19:12 -0500

    Added wooden box and some notes for how weapons should be stored and accessed

--- available_weapons.py
+++ available_weapons.py
@@ -5,3 +5,11 @@ old_rusty_knife = Weapons('Old Rusty Knife', 10, 'Melee', 1)
 
 wooden_staff = Weapons('Wooden Staff', 5, 'Magic', 1)
 
+wooden_bow = Weapons('Wooden Bow', 10, 'Range', 1)
+
+#these are good as placeholder but
+#possibly just have a weapons list access it like weapons_list[0] and have it
+# be filled from text file
+# read the file add it to the list
+# might have to have multiple lists for the different weapon types
+# one for range, one for melee, one for magic, one for range, etc.
