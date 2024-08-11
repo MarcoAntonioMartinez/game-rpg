eaf6ab11aa7739350657394c63cb9ad892892957
Marco Martinez <marco.antoni.martinez@gmail.com>
2024-08-11 13:40:34 -0500

    changed some of the comment in placeholder

--- placeholder.py
+++ placeholder.py
@@ -1,4 +1,4 @@
-# this will be the main game stuff where everything all comes together
+# this will be where the main gameplay loop will be where everything all comes together
 # rpg turn based text based
 # a bit of if statements
 #need to come up with a better name for the file
